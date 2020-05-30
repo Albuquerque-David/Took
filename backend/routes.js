1cd4a312e7daf1cca955cff698a0ec81aeac6e74
David <davidra_rj@hotmail.com>
2020-05-30 19:01:04 -0300

    Books crud

--- backend/routes.js
+++ backend/routes.js
@@ -4,6 +4,11 @@ const BookController = require('./src/database/controllers/BookController')
 
 const routes = express.Router();
 
-routes.post('/books',BookController.create)
+routes.post('/book',BookController.create)
+routes.get('/book', BookController.readById)
+routes.get('/books', BookController.readAll)
+routes.get('/book', BookController.searchByName)
+routes.put('/book', BookController.update)
+routes.delete('/book/:id',BookController.delet);
 
 module.exports = routes;
--- backend/src/database/controllers/BookController.js
+++ backend/src/database/controllers/BookController.js
@@ -1,3 +1,7 @@
+//TODO: TRATAMENTO DE EXCESSÕES E RETORNO DE ERROS HTTP
+//TODO: TRATAMENTO DE PERMISSÕES PRA FAZER AS MUDANÇAS
+
+
 var mongoose = require("mongoose")
 var { BookSchema } = require("../models/Book")
 const uuidModule = require('uuid')
@@ -14,8 +18,6 @@ var create = async (request, response) =>
         description
     })
 
-    console.log(newBook)
-
     newBook.save((error) => 
         {
             if(error)
@@ -29,19 +31,84 @@ var create = async (request, response) =>
     )
 }
 
-var update = async () => 
+var update = async (request, response) => 
 {
+    var { id, name, description } = request.body
 
+    const filter = { '_id': id }
+    const update = { name , description}
+
+    Book.findOneAndUpdate(filter,update, {new: true}).exec((error, result) =>
+    {
+        if(error)
+            console.log(error)
+        else
+        {  
+            console.log(result)
+            response.json({result})
+        }
+    })
 }
 
-var read = async () =>
+var readById = async (request,response) => 
 {
-
+    var { id } = request.query
+    Book.findById(id).exec((error,result) =>
+    {
+        if(error)
+            console.log(error)
+        else
+        {
+            console.log(result)
+            response.json({ result })
+        }
+    })
 }
 
-var delet = async () =>
+var searchByName = async (request, response) =>
 {
+    var { name } = request.query
 
+    Book.find({'name': name}).exec((error, result) => 
+    {
+        if(error)
+            console.log(error)
+        else
+        {
+            console.log(result)
+            response.json({ result })
+        }
+    })
 }
 
-module.exports = {create,update,read,delet}
+var readAll = async (request, response) =>
+{
+    Book.find({}).exec((error, result) => 
+    {
+        if(error)
+            console.log(error)
+        else
+        {
+            console.log(result)
+            response.json({ result })
+        }
+    })
+}
+
+var delet = async (request, response) =>
+{
+    var { id } = request.params
+
+    Book.findByIdAndDelete(id).exec((error, result) => 
+    {
+        if(error)
+            console.log(error)
+        else
+        {
+            console.log(result)
+            response.json({ result })
+        }
+    })
+}
+
+module.exports = {create,update,readById,searchByName,readAll,delet}
